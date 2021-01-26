0558f242222bea22c4d2700ae36f48509d0d2419
GuiCoelho <guiminervino@gmail.com>
2021-01-26 01:34:37 -0300

    asdasd

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -10,7 +10,7 @@ function IndexPage() {
 
       <meta property="og:title" content="Lolinho" key="ogtitle" />
       <meta property="og:description" content="Iupi" key="ogdesc" />
-      <meta property="og:image" content="https://i.pinimg.com/originals/bd/4c/23/bd4c236086fa1bb4bbcc8e6cdffdf785.png" key="ogima ge" />
+      <meta property="og:image" content="https://i.pinimg.com/originals/bd/4c/23/bd4c236086fa1bb4bbcc8e6cdffdf785.png" key="ogimage" />
     </Head>
 
 
