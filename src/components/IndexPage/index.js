a874673d060a87e045790c9f2d5a5a43922084c6
GuiCoelho <guiminervino@gmail.com>
2021-01-26 01:35:52 -0300

    Tentativa

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -8,9 +8,9 @@ function IndexPage() {
       <title>Lol quiz</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-      <meta property="og:title" content="Lolinho" key="ogtitle" />
+      <meta property="og:title" content="Tentativa" key="ogtitle" />
       <meta property="og:description" content="Iupi" key="ogdesc" />
-      <meta property="og:image" content="https://i.pinimg.com/originals/bd/4c/23/bd4c236086fa1bb4bbcc8e6cdffdf785.png" key="ogimage" />
+      <meta property="og:image" content={db.bg} key="ogimage" />
     </Head>
 
 
