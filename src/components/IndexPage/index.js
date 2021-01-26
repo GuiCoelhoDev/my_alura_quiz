1f09bf767ad3a774369909738c1f2e6897617ccc
GuiCoelho <guiminervino@gmail.com>
2021-01-26 01:32:28 -0300

    Tentativa de usar og:img

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -3,14 +3,17 @@ import db from "../../../db.json"
 
 function IndexPage() {
   return (
-   
-      <Head>
-        <title>Lol quiz</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:image" content="https://i.pinimg.com/originals/bd/4c/23/bd4c236086fa1bb4bbcc8e6cdffdf785.png"/>
-      </Head>
-     
-  
+
+    <Head>
+      <title>Lol quiz</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <meta property="og:title" content="Lolinho" key="ogtitle" />
+      <meta property="og:description" content="Iupi" key="ogdesc" />
+      <meta property="og:image" content="https://i.pinimg.com/originals/bd/4c/23/bd4c236086fa1bb4bbcc8e6cdffdf785.png" key="ogima ge" />
+    </Head>
+
+
   )
 }
 
