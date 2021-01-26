3b7893e506f25366b92dd85ac12cc1f51db5770c
GuiCoelho <guiminervino@gmail.com>
2021-01-26 00:40:31 -0300

    Aula 1 Alura Imersao

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
 import styled from 'styled-components'
+import Widget from '../src/components/Widget'
+import Footer from "../src/components/Footer"
+import GitHubCorner from "../src/components/GitHubCorner"
+import QuizBackground from "../src/components/QuizBackground"
+//TODO: 1- Colocar background
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+import db from "../db.json"
+
+const BackGroundImage = styled.div`
+  background-image: url("${db.bg}");
+  flex: 1;  
+  background-size: cover;
+  background-position: center;
+`
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+
+            <h1>Lolzinho</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Runas
+             </p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+            <Widget.Header>
+
+              <h1>Hehehe</h1>
+            </Widget.Header>
+          <Widget.Content>
+            <p>
+              ASDHAH
+            </p>
+
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner/>
+    </QuizBackground >
+
+
+  )
 }
+
